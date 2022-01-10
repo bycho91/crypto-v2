353e38b7778f2d2efd588943cbe25c01403b9872
botmon21 <baeyuncho@gmail.com>
2022-01-10 12:53:53 -0600

    added crypto cards

--- src/components/CoinCard/CoinCard.js
+++ src/components/CoinCard/CoinCard.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./CoinCard.scss";
 
+import millify from "millify";
+
 const CoinCard = ({ coin }) => {
   return (
-    <div>
-      <h1>{coin.name}</h1>
+    <div className="coin-card">
+      <div className="coin-card-header">
+        <div className="coin-card-header-info">
+          <img src={coin.iconUrl} alt={coin.name} />
+          <h2>{coin.name}</h2>
+          <p>{coin.symbol}</p>
+        </div>
+        <p>Rank: {coin.rank}</p>
+      </div>
+
+      <div className="coin-card-price">
+        <h1>${parseFloat(coin.price).toFixed(2)}</h1>
+        <div className="market-cap">Market Cap: ${millify(coin.marketCap)}</div>
+        <div className="rank">Rank: </div>
+      </div>
     </div>
   );
 };
